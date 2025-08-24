1ac18d65d518ab5098bd813dd7d1c6224fe57d8f
wnpcode <->
2025-08-25 06:55:19 +0700

    -

--- app/page.tsx
+++ app/page.tsx
@@ -251,8 +251,8 @@ export default function Home() {
                 <div className="grid md:grid-cols-2 gap-12">
                   <div>
                     <Image
-                      src=""
-                      // src="/mempelai-wanita.resized.JPG"
+                      // src=""
+                      src="/mempelai-wanita.resized.JPG"
                       alt="Mempelai Wanita"
                       width={200}
                       height={200}
@@ -265,8 +265,8 @@ export default function Home() {
                   </div>
                   <div>
                     <Image
-                      src=""
-                      // src="/mempelai-pria.resized.JPG"
+                      // src=""
+                      src="/mempelai-pria.resized.JPG"
                       alt="Mempelai Pria"
                       width={200}
                       height={200}
